2c96dbef86f8709d4171311c40e529e6464b3e4f
Peppe289 <gsperanza204@protonmail.com>
2024-12-23 13:19:03 +0100

    Activity: use MainActivity as access page
    then create DispatcherActivity for manage page from bottomBar
    
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

---
+++ app/src/main/java/com/peppe289/echotrail/DispatcherActivity.java
@@ -0,0 +1,42 @@
+package com.peppe289.echotrail;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.peppe289.echotrail.databinding.ActivityDispatcherBinding;
+import com.peppe289.echotrail.model.BottomBar;
+
+public class DispatcherActivity extends AppCompatActivity {
+    protected ActivityDispatcherBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dispatcher);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dispatcher), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        /*
+          This will fix app crash for:
+          - java.lang.IllegalStateException: FragmentManager has not been attached to a host.
+         */
+        BottomBar.setActivity(this);
+
+        binding = ActivityDispatcherBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        BottomBar.loadFragment(new MapFragment());
+
+        // create a listener for the bottom navigation bar
+        binding.bottomNavigationView.setOnItemSelectedListener(BottomBar::onNavigationItemSelected);
+    }
+}
--- app/src/main/java/com/peppe289/echotrail/MainActivity.java
+++ app/src/main/java/com/peppe289/echotrail/MainActivity.java
@@ -1,5 +1,6 @@
 package com.peppe289.echotrail;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,33 +10,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.peppe289.echotrail.databinding.ActivityMainBinding;
-import com.peppe289.echotrail.model.BottomBar;
 
 public class MainActivity extends AppCompatActivity {
-    protected ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        /*
-          This will fix app crash for:
-          - java.lang.IllegalStateException: FragmentManager has not been attached to a host.
-         */
-        BottomBar.setActivity(this);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        BottomBar.loadFragment(new MapFragment());
-
-        // create a listener for the bottom navigation bar
-        binding.bottomNavigationView.setOnItemSelectedListener(BottomBar::onNavigationItemSelected);
+        binding.guestAccessBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, DispatcherActivity.class);
+            startActivity(intent);
+        });
     }
 }
