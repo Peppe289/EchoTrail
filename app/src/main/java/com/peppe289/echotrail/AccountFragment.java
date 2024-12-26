56a0af8a118bf847c4c22c73d6c3b305984f3858
Peppe289 <gsperanza204@protonmail.com>
2024-12-26 14:05:12 +0100

    rework Account page
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- app/src/main/java/com/peppe289/echotrail/AccountFragment.java
+++ app/src/main/java/com/peppe289/echotrail/AccountFragment.java
@@ -1,16 +1,16 @@
 package com.peppe289.echotrail;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.peppe289.echotrail.controller.user.UserController;
+import com.peppe289.echotrail.databinding.FragmentAccountBinding;
 import com.peppe289.echotrail.utils.MoveActivity;
 
 /**
@@ -18,22 +18,23 @@ import com.peppe289.echotrail.utils.MoveActivity;
  * create an instance of this fragment.
  */
 public class AccountFragment extends Fragment {
+    private FragmentAccountBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_account, container, false);
-        Button logoutBtn = rootView.findViewById(R.id.logoutBtn);
-        logoutBtn.setOnClickListener(view -> {
+        binding = FragmentAccountBinding.inflate(inflater, container, false);
+        View rootView = binding.getRoot();
+
+        // TODO: implements the button function (should be open personal notes list)
+        binding.mynotes.setOnClickListener(view -> Log.i("AccountFragment", "My notes button clicked"));
+
+        binding.logoutBtn.setOnClickListener(view -> {
             UserController.logout();
             MoveActivity.rebaseActivity(getActivity(), MainActivity.class);
             Log.i("AccountFragment", "User logged out");
         });
+
         return rootView;
     }
 }
