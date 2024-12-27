572db742c3f132c8dd21ae5882e4e8929c410a58
Peppe289 <gsperanza204@protonmail.com>
2024-12-27 09:10:38 +0100

    UserControll: refactor
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- app/src/main/java/com/peppe289/echotrail/controller/user/UserController.java
+++ app/src/main/java/com/peppe289/echotrail/controller/user/UserController.java
@@ -16,30 +16,41 @@ import java.util.HashMap;
  */
 public class UserController {
 
-    public interface UserHeadersCallback {
-        void onComplete(HashMap<String, String> headers);
-    }
-
     /**
      * Logs in the user using an email and password.
      *
      * @param email    The user's email address.
      * @param password The user's password.
+     * @param callback The callback to be invoked upon completion.
+     * @throws UserStateException if the user is already signed in.
      */
     public static void login(String email, String password, UserDAO.SignInCallback callback) {
-        UserDAO.signIn(email, password, callback);
+        validateCallback(callback);
+
+        if (!isLoggedIn()) {
+            UserDAO.signIn(email, password, callback);
+        } else {
+            throw new UserStateException("User is already signed in.");
+        }
     }
 
     /**
-     * Registers a new user with the specified email and password.
+     * Registers a new user with the specified email, password, and username.
      *
      * @param email    The user's email address.
      * @param password The user's password.
      * @param username The user's username.
      * @param callback The callback to be invoked upon completion.
+     * @throws UserStateException if the user is already signed in.
      */
     public static void register(String email, String password, String username, UserDAO.SignUpCallback callback) {
-        UserDAO.signUp(email, password, username, callback);
+        validateCallback(callback);
+
+        if (!isLoggedIn()) {
+            UserDAO.signUp(email, password, username, callback);
+        } else {
+            throw new UserStateException("User is already signed in.");
+        }
     }
 
     /**
@@ -53,35 +64,93 @@ public class UserController {
 
     /**
      * Logs out the currently authenticated user.
+     *
+     * @param context The context to access shared preferences.
+     * @throws UserStateException if the user is not signed in.
      */
     public static void logout(Context context) {
-        UserDAO.signOut();
-        // clear user headers from shared preferences
-        PreferencesHelper.clearUserHeaders(context);
+        if (isLoggedIn()) {
+            UserDAO.signOut();
+            PreferencesHelper.clearUserHeaders(context); // Clear user headers from shared preferences
+        } else {
+            throw new UserStateException("User is not signed in.");
+        }
     }
 
     /**
      * Retrieves the username of the currently authenticated user.
      *
      * @param callback The callback to be invoked upon completion.
+     * @throws UserStateException if the user is not signed in.
      */
     public static void getUsername(UserDAO.UpdateUsernameViewCallback callback) {
-        UserDAO.getUsername(callback);
+        validateCallback(callback);
+
+        if (isLoggedIn()) {
+            UserDAO.getUsername(callback);
+        } else {
+            throw new UserStateException("User is not signed in.");
+        }
     }
 
     /**
      * Retrieves the email of the currently authenticated user.
      *
      * @param callback The callback to be invoked upon completion.
+     * @throws UserStateException if the user is not signed in.
      */
     public static void getEmail(UserDAO.UpdateEmailViewCallback callback) {
-        UserDAO.getEmail(callback);
+        validateCallback(callback);
+
+        if (isLoggedIn()) {
+            UserDAO.getEmail(callback);
+        } else {
+            throw new UserStateException("User is not signed in.");
+        }
     }
 
     /**
-     * Retrieves the user headers from the shared preferences.
+     * Retrieves the user headers from shared preferences.
+     *
+     * @param context  The context to access shared preferences.
+     * @param callback The callback to be invoked with the user headers.
+     * @throws UserStateException if the user is not signed in.
      */
     public static void getUserHeadersFromPreferences(Context context, UserHeadersCallback callback) {
-        PreferencesHelper.checkOnPreferences(context, callback);
+        validateCallback(callback);
+
+        if (isLoggedIn()) {
+            PreferencesHelper.checkOnPreferences(context, callback);
+        } else {
+            throw new UserStateException("User is not signed in.");
+        }
+    }
+
+    /**
+     * Validates that the provided callback is not null.
+     *
+     * @param callback The callback to validate.
+     * @throws IllegalArgumentException if the callback is null.
+     */
+    private static void validateCallback(Object callback) {
+        if (callback == null) {
+            throw new IllegalArgumentException("Callback cannot be null.");
+        }
+    }
+
+    /**
+     * Callback interface for retrieving user headers.
+     */
+    public interface UserHeadersCallback {
+        void onComplete(HashMap<String, String> headers);
+    }
+
+    /**
+     * Custom exception class for handling user state-related errors.
+     */
+    public static class UserStateException extends RuntimeException {
+        public UserStateException(String message) {
+            super(message);
+        }
     }
 }
