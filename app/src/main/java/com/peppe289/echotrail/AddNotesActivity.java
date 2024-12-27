7772f7c00f72ddbaea572ecd08224a9f79122ff3
Peppe289 <gsperanza204@protonmail.com>
2024-12-27 11:54:25 +0100

    Notes: configure toolbar
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- app/src/main/java/com/peppe289/echotrail/AddNotesActivity.java
+++ app/src/main/java/com/peppe289/echotrail/AddNotesActivity.java
@@ -1,6 +1,9 @@
 package com.peppe289.echotrail;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.peppe289.echotrail.databinding.ActivityAddNotesBinding;
 
 public class AddNotesActivity extends AppCompatActivity {
@@ -27,5 +31,37 @@ public class AddNotesActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setUpToolBar();
+    }
+
+    private void setUpToolBar() {
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        toolbar.setNavigationOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_notes_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_save) saveNote();
+        else return super.onOptionsItemSelected(item);
+
+        return true;
+    }
+
+    private void saveNote() {
+        Toast.makeText(this, "Nota salvata!", Toast.LENGTH_SHORT).show();
     }
 }
