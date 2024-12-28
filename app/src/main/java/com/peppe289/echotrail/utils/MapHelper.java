3f0c4b55b5b24667b411742f4046e96a171dd554
Peppe289 <gsperanza204@protonmail.com>
2024-12-28 18:46:56 +0100

    MapFragment: put marker at user location
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- app/src/main/java/com/peppe289/echotrail/utils/MapHelper.java
+++ app/src/main/java/com/peppe289/echotrail/utils/MapHelper.java
@@ -5,12 +5,15 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 
 public class MapHelper {
-    private MapView mapView;
+    private final MapView mapView;
+    private Marker marker;
 
     public MapHelper(MapView mapView) {
         this.mapView = mapView;
+        marker = null;
     }
 
     public void initializeMap(Context context) {
@@ -26,6 +29,16 @@ public class MapHelper {
     public void setMapCenter(GeoPoint point) {
         if (mapView != null) {
             mapView.getController().setCenter(point);
+            if (marker == null) {
+                marker = new Marker(mapView);
+                marker.setTitle("La mia posizione");
+                //marker.setIcon(mapView.getContext().getDrawable(android.R.drawable.ic_menu_mylocation));
+                //marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                marker.setPosition(point);
+                mapView.getOverlays().add(marker);
+            } else {
+                marker.setPosition(point);
+            }
         }
     }
 
