df54fed3d3967f2c0d44896571596d7b969a04d1
Peppe289 <gsperanza204@protonmail.com>
2024-12-28 21:39:29 +0100

    NotesFragment: add template for list of notes
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- app/src/main/java/com/peppe289/echotrail/NotesFragment.java
+++ app/src/main/java/com/peppe289/echotrail/NotesFragment.java
@@ -7,58 +7,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NotesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class NotesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NotesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NotesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NotesFragment newInstance(String param1, String param2) {
-        NotesFragment fragment = new NotesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes, container, false);
+        View view = inflater.inflate(R.layout.fragment_notes, container, false);
+
+        LinearLayout cardContainer = view.findViewById(R.id.card_container);
+
+        String[] titles = {"Titolo 1", "Titolo 2", "Titolo 3", "Titolo 1", "Titolo 2", "Titolo 3", "Titolo 1", "Titolo 2", "Titolo 3"};
+        String[] descriptions = {"Descrizione 1", "Descrizione 2", "Descrizione 3", "Descrizione 1", "Descrizione 2", "Descrizione 3", "Descrizione 1", "Descrizione 2", "Descrizione 3"};
+
+        for (int i = 0; i < titles.length; i++) {
+            View card = LayoutInflater.from(requireContext()).inflate(R.layout.card_item, cardContainer, false);
+
+            ViewHolder viewHolder = new ViewHolder(card);
+            viewHolder.title.setText(titles[i]);
+            viewHolder.description.setText(descriptions[i]);
+
+            cardContainer.addView(card);
+        }
+
+        return view;
+    }
+
+    static class ViewHolder {
+        TextView title, description;
+
+        public ViewHolder(View cardView) {
+            this.title = cardView.findViewById(R.id.authors);
+            this.description = cardView.findViewById(R.id.content);
+        }
     }
 }
