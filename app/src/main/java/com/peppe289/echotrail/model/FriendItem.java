70543c3a33fca14b55b288de81bc82fa0809f306
Peppe289 <gsperanza204@protonmail.com>
2025-01-28 00:49:12 +0100

    Friends: add model.

---
+++ app/src/main/java/com/peppe289/echotrail/model/FriendItem.java
@@ -0,0 +1,47 @@
+package com.peppe289.echotrail.model;
+
+public class FriendItem {
+    private String name;
+    private boolean onPendingRequest;
+    private boolean isFriend;
+    private String uid;
+
+    public FriendItem(String name, boolean onPendingRequest, boolean isFriend, String uid) {
+        this.name = name;
+        this.onPendingRequest = onPendingRequest;
+        this.isFriend = isFriend;
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isOnPendingRequest() {
+        return onPendingRequest;
+    }
+
+    public void setOnPendingRequest(boolean onPendingRequest) {
+        this.onPendingRequest = onPendingRequest;
+    }
+
+    public boolean isFriend() {
+        return isFriend;
+    }
+
+    public void setFriend(boolean friend) {
+        isFriend = friend;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
