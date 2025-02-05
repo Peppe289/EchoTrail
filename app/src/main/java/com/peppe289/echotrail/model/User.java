1d6161ed39f16a7012f983bc05d0e58cbd9b2dbd
Peppe289 <gsperanza204@protonmail.com>
2025-02-05 17:29:32 +0100

    model: create a user model

---
+++ app/src/main/java/com/peppe289/echotrail/model/User.java
@@ -0,0 +1,73 @@
+package com.peppe289.echotrail.model;
+
+import java.util.List;
+
+public class User {
+    private String username;
+    private List<String> links;
+    private List<String> notes;
+    private List<String> readedNotes;
+    private List<String> friends;
+    private boolean anonymousByDefault;
+
+    public User() {
+
+    }
+
+    public User(String username, List<String> links, List<String> notes, List<String> readedNotes, List<String> friends, boolean anonymousByDefault) {
+        this.username = username;
+        this.links = links;
+        this.notes = notes;
+        this.readedNotes = readedNotes;
+        this.friends = friends;
+        this.anonymousByDefault = anonymousByDefault;
+    }
+
+    public boolean isAnonymousByDefault() {
+        return anonymousByDefault;
+    }
+
+    public void setAnonymousByDefault(boolean anonymousByDefault) {
+        this.anonymousByDefault = anonymousByDefault;
+    }
+
+    public List<String> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<String> friends) {
+        this.friends = friends;
+    }
+
+    public List<String> getReadedNotes() {
+        return readedNotes;
+    }
+
+    public void setReadedNotes(List<String> readedNotes) {
+        this.readedNotes = readedNotes;
+    }
+
+    public List<String> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<String> notes) {
+        this.notes = notes;
+    }
+
+    public List<String> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<String> links) {
+        this.links = links;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
