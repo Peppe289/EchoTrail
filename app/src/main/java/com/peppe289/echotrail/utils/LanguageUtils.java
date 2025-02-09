33ff75cb05ac1e8a891be39e9f520dd7a5010101
Peppe289 <gsperanza204@protonmail.com>
2025-02-09 15:40:26 +0100

    LanguageUtils: update function

--- app/src/main/java/com/peppe289/echotrail/utils/LanguageUtils.java
+++ app/src/main/java/com/peppe289/echotrail/utils/LanguageUtils.java
@@ -4,16 +4,48 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import com.peppe289.echotrail.R;
+import com.peppe289.echotrail.controller.user.PreferencesController;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class LanguageUtils {
-    public static void setLocale(Context context, Locale locale) {
-        Resources res = context.getResources();
-        Configuration config = res.getConfiguration();
-        config.setLocale(locale);
-        context.createConfigurationContext(config);
+    public static Context setAppLanguage(Context context, String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+
+        Resources resources = context.getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.setLocale(locale);
+
+        PreferencesController.setLanguages(languageCode);
+
+        return context.createConfigurationContext(configuration);
+    }
+
+    public static Context setAppLanguage(Context context) {
+        Locale locale;
+
+        String languageCode = PreferencesController.getLanguages();
+
+        locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+
+        Resources resources = context.getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.setLocale(locale);
+
+        PreferencesController.setLanguages(languageCode);
+
+        return context.createConfigurationContext(configuration);
+    }
+
+    public static List<Locale> getAvailableLocales() {
+        Locale[] testLocales = {
+                Locale.ENGLISH, Locale.ITALIAN
+        };
+
+        return List.of(testLocales);
     }
 }
