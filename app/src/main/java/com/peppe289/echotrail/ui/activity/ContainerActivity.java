f7cdc49bf62198e612b7b3871e630261f3cc9bb1
Peppe289 <gsperanza204@protonmail.com>
2025-02-09 17:23:56 +0100

    ContainerActivity: add navbar and fix layout

--- app/src/main/java/com/peppe289/echotrail/ui/activity/ContainerActivity.java
+++ app/src/main/java/com/peppe289/echotrail/ui/activity/ContainerActivity.java
@@ -1,8 +1,15 @@
 package com.peppe289.echotrail.ui.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.peppe289.echotrail.R;
 import com.peppe289.echotrail.databinding.ActivityContainerBinding;
 
@@ -19,6 +26,23 @@ public class ContainerActivity extends AppCompatActivity {
                 ActivityContainerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        MaterialToolbar toolbar;
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+
         // Check if the fragment is already in the container.
         if (savedInstanceState == null) {
             String fragmentName = getIntent().getExtras().getString(FRAGMENT_KEY);
@@ -33,6 +57,15 @@ public class ContainerActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getOnBackPressedDispatcher().onBackPressed(); // Torna alla schermata precedente
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
