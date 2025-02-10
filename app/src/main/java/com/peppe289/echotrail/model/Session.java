0d295a40ab38daa7de337e0c7a5e0acd513f58de
Peppe289 <gsperanza204@protonmail.com>
2025-02-10 21:09:08 +0100

    Session: create session class

---
+++ app/src/main/java/com/peppe289/echotrail/model/Session.java
@@ -0,0 +1,49 @@
+package com.peppe289.echotrail.model;
+
+import com.google.firebase.Timestamp;
+
+public class Session {
+    public String device;
+    public String position;
+    public Timestamp time;
+    public String version;
+
+    public Session(String device, String position, Timestamp time, String version) {
+        this.device = device;
+        this.position = position;
+        this.time = time;
+        this.version = version;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
